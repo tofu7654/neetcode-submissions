f5de74c19ca302ecbf414921144c2dc4f61cca71
Toufeeq Sharieff <toufeeqsharieff0@gmail.com>
2026-08-05 21:09:14 -0400

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        prevMap = {}
+
+        for i, n in enumerate(nums):
+            search = target - n
+
+            if search in prevMap:
+                return [prevMap[search], i]
+            
+            prevMap[n] = i
+        
+        return
