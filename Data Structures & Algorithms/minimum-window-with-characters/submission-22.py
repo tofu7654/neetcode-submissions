1c22b8c6c1c6da27b4e5162298201ef928ac5e31
Toufeeq Sharieff <toufeeqsharieff0@gmail.com>
2026-09-10 18:30:18 -0400

    Add: minimum-window-with-characters - submission-22

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-22.py
@@ -0,0 +1,44 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        freqW, freqT = {}, {}
+        l = 0
+        lengthShortest, shortest = float("infinity"), [-1, -1]
+
+        # edge case where T is empty 
+        if not t:
+            return ""
+
+        # build freq map of T
+        for c in t:
+            freqT[c] = freqT.get(c, 0) + 1
+
+        have = 0
+        need = len(freqT)
+        for r in range(len(s)):
+            c = s[r]
+            freqW[c] = freqW.get(c, 0) + 1
+
+            if c in freqT:
+                # check if condition is met
+                if freqW[c] == freqT[c]:
+                    have += 1
+            
+            # if have == need, make invalid
+            while have == need:
+                if r - l + 1 < lengthShortest:
+                    shortest = [l, r]
+                    lengthShortest = r - l + 1
+                
+                # make window invalid
+                freqW[s[l]] -= 1
+                if s[l] in freqT and freqW[s[l]] < freqT[s[l]]:
+                    have -= 1
+                
+                l += 1
+            
+        l, r = shortest
+        return s[l:r + 1] if lengthShortest != float("infinity") else ""
+
+
+
+        
