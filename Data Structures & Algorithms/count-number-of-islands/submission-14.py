ada05715a5f76e800bb023030f9c54c7eae534a3
Toufeeq Sharieff <toufeeqsharieff0@gmail.com>
2026-09-20 22:34:34 -0400

    Add: count-number-of-islands - submission-14

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-14.py
@@ -0,0 +1,36 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
+
+        rows, cols = len(grid), len(grid[0])
+        visited = set()
+        islands = 0
+
+        def bfs(r,c):
+            q = deque()
+            visited.add((r,c))
+            q.append((r,c))
+            while q:
+                row, col = q.pop()
+                directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+                for dr, dc in directions:
+                    r, c = row + dr, col + dc
+                    if (r in range(rows) and 
+                        c in range(cols) and 
+                        (r,c) not in visited and 
+                        grid[r][c] == '1'):
+
+                            # add this to visited and the queue
+                            visited.add((r,c))
+                            q.append((r,c))
+
+        # find an island we haven't found
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1" and (r,c) not in visited:
+                    # find out how big this island is w a bfs
+                    bfs(r,c)
+                    islands += 1
+        
+        return islands
